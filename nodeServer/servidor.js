af0db7d243ad1bbdbaa6a2253db98dbff31388a3
Alex Trujillo Vera <aleztrujillo91@hotmail.com>
2023-11-07 22:19:40 -0500

    V 1.6

--- nodeServer/servidor.js
+++ nodeServer/servidor.js
@@ -1,1 +1,31 @@
-const express =
+const { MongoClient } = require ("mongodb");
+
+const express = require("express");
+const server = express();
+const PORT = 3000;
+const path = require("path");
+const colors = require("colors");
+
+const connection = new MongoClient("mongodb://localhost:27017");
+
+async function iniciarDB(){
+
+try{
+let user = {
+    nombre: "",
+    apellidos: "",
+};
+connection.connect();
+connection.db("ProyectoFinal").collection("users").insertOne(user);
+} catch (e) {
+
+}}
+
+
+server.get("/", (req, res) =>{
+    res.sendfile(path,resolvePath("./index.html"));
+});
+server.get("/home", (req,res)=>{
+    res.sendfile(path.resolvePath("./index.html"));
+})
+server.listen(3000);
